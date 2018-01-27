2da27fbc9a906b260bc93dbabddf671e06b09cb5
Benjamin Schug <benjaminschug@gmail.com>
2018-01-27 21:59:06 +0100

    Add YtoZ script

---
+++ Assets/YToZ.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class YToZ : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var position = transform.position;
+        position.z = position.y / 1000f;
+        transform.position = position;
+	}
+}
