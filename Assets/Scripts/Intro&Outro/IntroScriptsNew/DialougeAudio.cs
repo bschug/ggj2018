51b0672c31beab9bae4181cf39471091e2861a5f
Mascha Camino <Maschi@marias-air.student.berlin.sae>
2018-01-27 18:50:29 +0100

    Walking

---
+++ Assets/Scripts/Intro&Outro/IntroScriptsNew/DialougeAudio.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialougeAudio : MonoBehaviour {
+
+	public class Dialouge{
+		
+		public AudioClip[] sentences; 
+	}
+}
---
+++ Assets/Scripts/Intro&Outro/IntroScriptsNew/DialougeManager_Audio.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialougeManager_Audio : MonoBehaviour {
+
+	public AudioClip m_dialougeAudio;
+	public Animator animator; 
+	public GameObject continueButton; 
+	public IntroScript_audio _introAudio; 
+	bool dialougeHasStarted; 
+	bool dialougeHasEnded = false; 
+
+	private Queue <AudioClip> sentences; 
+
+	// Use this for initialization
+	void Start () {
+
+		sentences = new Queue <AudioClip> (); 
+	}
+
+	public void playNextSentence(){
+
+		if (sentences.Count == 0) {
+
+			EndDialouge (); 
+			return; 
+		}
+
+		AudioClip sentence = sentences.Dequeue (); 
+		m_dialougeAudio = sentence; 
+	}
+
+
+	public void EndDialouge(){
+
+
+	}
+}
