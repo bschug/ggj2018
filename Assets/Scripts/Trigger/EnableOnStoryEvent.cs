b6968b7647d2fb5e8304a538f2420af99553dc52
Benjamin Schug <benjaminschug@gmail.com>
2018-01-28 14:26:40 +0100

    Enable/DisableOnStoryEvent

---
+++ Assets/Scripts/Trigger/EnableOnStoryEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableOnStoryEvent : MonoBehaviour
+{
+    public string StoryEvent;
+
+    private void Awake()
+    {
+        var storyManager = FindObjectOfType<StoryManager>();
+        var hasBeenTriggered = storyManager.hasBeenTriggered.Contains(StoryEvent);
+        gameObject.SetActive(hasBeenTriggered);
+    }
+}
